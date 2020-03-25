b4b658ed7c73d3d9866061c1099a15e4ffb12f84
runzexia <runzexia@yunify.com>
2020-03-25 10:57:51 +0800

    add pipeline controller
    Signed-off-by: runzexia <runzexia@yunify.com>

--- pkg/apis/devops/v1alpha3/pipeline_types.go
+++ pkg/apis/devops/v1alpha3/pipeline_types.go
@@ -23,6 +23,14 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+const PipelineFinalizerName = "pipeline.finalizers.kubesphere.io"
+
+const (
+	ResourceKindPipeline     = "Pipeline"
+	ResourceSingularPipeline = "pipeline"
+	ResourcePluralPipeline   = "pipelines"
+)
+
 // PipelineSpec defines the desired state of Pipeline
 type PipelineSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
